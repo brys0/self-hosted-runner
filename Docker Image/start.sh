b344f59f88b22eb99df77be77b84e49f3840dc0a
Brys <35676267+brys0@users.noreply.github.com>
2025-01-17 12:44:58 -0600

    Update start.sh

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -4,7 +4,7 @@ REPO=$REPO
 REG_TOKEN=$REG_TOKEN
 NAME=$NAME
 
-cd /home/docker/actions-runner || exit
+cd /home/docker || exit
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
 
 cleanup() {
