8297fb9c9c4bc5db6d938bdac195bcd4e76e77f7
Brys <35676267+brys0@users.noreply.github.com>
2025-01-17 12:47:16 -0600

    Update start.sh

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -7,12 +7,4 @@ NAME=$NAME
 cd /home/docker || exit
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
 
-cleanup() {
-  echo "Removing runner..."
-  ./config.sh remove --unattended --token ${REG_TOKEN}
-}
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-./run.sh & wait $!
+./run.sh
